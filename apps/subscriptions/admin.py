aadf50cfecdeb8bf953772ba32020d4efaa1d884
Paul Ndambo <52633424+Paulndambo@users.noreply.github.com>
2024-02-23 15:09:45 +0300

    db flushed

--- apps/subscriptions/admin.py
+++ apps/subscriptions/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from apps.subscriptions.models import Pricing
+from apps.subscriptions.models import Pricing, Subscription
 
 # Register your models here.
 admin.site.register(Pricing)
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "package", "status", "start_date", "end_date"]
--- apps/users/admin.py
+++ apps/users/admin.py
@@ -6,4 +6,4 @@ from apps.users.models import User
 # Register your models here.
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ["id", "username", "name", "email", "role", "token"]
+    list_display = ["id", "username", "is_active", "activated", "name", "email", "role", "token"]
